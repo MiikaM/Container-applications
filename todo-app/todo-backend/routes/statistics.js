040b0c4adae41f2e5369fac234acbbca5ebb1613
MiikaM <miika.mikkonen5@gmail.com>
2023-01-10 13:18:53 +0200

    12.10-.11: done

--- todo-app/todo-backend/routes/statistics.js
+++ todo-app/todo-backend/routes/statistics.js
@@ -6,14 +6,12 @@ const configs = require('../util/config')
 const redis = require('../redis')
 let visits = 0
 
-/* GET statistics data. */
+/* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
-
-  res.send({
-    ...configs,
-    visits
-  });
+    const result = await redis.getAsync('added_todos');
+    res.send({
+        "added_todos": result
+    });
 });
 
 module.exports = router;
--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Todo } = require('../mongo')
 const router = express.Router();
+const {setAsync, getAsync} = require('../redis')
 
 /* GET todos listing. */
 router.get('/', async (_, res) => {
@@ -14,6 +15,10 @@ router.post('/', async (req, res) => {
     text: req.body.text,
     done: false
   })
+  if (todo) {
+    const result = await getAsync('added_todos') ?? 0; 
+    const addition = await setAsync('added_todos', parseInt(result) + 1 );
+  }
   res.send(todo);
 });
 
